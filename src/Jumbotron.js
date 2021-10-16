7298b4c37bfe0fa030cb804db9edd33c1bedafd7
FrandyGuerra <frandyguerra2012@gmail.com>
2021-10-16 01:00:55 +0000

    Separar jumbotron de nav

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Jumbotron=()=>{
     return(
 
-        <div class="jumbotron mb-5 border rounded p-4 bg-light">
+        <div class="jumbotron mb-5 border rounded p-4 bg-light mt-3">
           <h1 class="display-4">A Warm Welcome!</h1>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
           <p class="lead">
